22947ec10d28b640fa44563b3421e70e51adf2d5
Iko Almas <90682984+IkoAlmasDevGame@users.noreply.github.com>
2025-03-21 15:14:32 +0700

    Update header.php

--- public/ui/header.php
+++ public/ui/header.php
@@ -5,7 +5,6 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <?php session_start(); ?>
-      <?php require_once("../../../app/init.php"); ?>
       <?php require_once("../route/route.php"); ?>
       <title><?php echo " - Beranda"; ?></title>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
