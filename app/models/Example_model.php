89b95cba84c7a004469963ed32273b3ff48c82b1
Iko Almas <90682984+IkoAlmasDevGame@users.noreply.github.com>
2025-08-17 09:26:09 +0700

    Update Example_model.php

--- app/models/Example_model.php
+++ app/models/Example_model.php
@@ -8,9 +8,18 @@ class Example_model
 {
    protected $tabel = ""; # database in website phpmyadmin
    protected $dbh;
+   private static $instance = null;
 
    public function __construct()
    {
       $this->dbh = new Database;
+   }
+
+   public static function getInstance()
+   {
+      if (self::$instance == null) {
+         self::$instance = new Example_model();
+      }
+      return self::$instance;
    }
 }
